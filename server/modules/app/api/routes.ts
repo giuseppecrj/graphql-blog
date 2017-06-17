3d281909211c0ecfb1230578b597a020ddbc619b
Herman-Scheer <g@herman-scheer.com>
2017-06-16 18:05:11 -0700

    subscriptions

--- server/modules/app/api/routes.ts
+++ server/modules/app/api/routes.ts
@@ -1,6 +1,9 @@
 import { makeExecutableSchema } from 'graphql-tools'
 import { Author } from './authors/authors.server.schema'
 import { Post } from './post/post.server.schema'
+import { PubSub } from 'graphql-subscriptions'
+
+export const pubsub = new PubSub()
 
 const Types = () => [Author, Post]
 
@@ -15,13 +18,24 @@ const Query = `
   type Mutation {
     addPost (authorId: Int!, title: String!): Post
   }
+
+  type Subscription {
+    somethingChanged: Result
+  }
+
+  type Result {
+    id: String
+  }
 `
 const SchemaDefinition = `
   schema {
     query: Query
-    mutation: Mutation
+    mutation: Mutation,
+    subscription: Subscription
   }
 `
+
+const SOMETHING_CHANGED_TOPIC = 'something_changed'
 
 const resolvers = {
   Query: {
@@ -32,6 +46,11 @@ const resolvers = {
   Mutation: {
     addPost: (_, args, ctx) => ctx.posts.create(args)
   },
+  Subscription: {
+    somethingChanged: {
+      subscribe: () => pubsub.asyncIterator(SOMETHING_CHANGED_TOPIC)
+    }
+  },
   Author: {
     posts: (author, args, ctx) => ctx.posts.find(author.id)
   },
@@ -39,6 +58,10 @@ const resolvers = {
     author: (post, args, ctx) => ctx.authors.find(post.authorId)
   }
 }
+
+// setTimeout(() => {
+//   pubsub.publish(SOMETHING_CHANGED_TOPIC, { somethingChanged: { id: '123' } })
+// }, 5000)
 
 export const Schema = makeExecutableSchema({
   typeDefs: [
